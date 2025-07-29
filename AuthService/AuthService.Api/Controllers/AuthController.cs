f7097582fd6ff0ff5654405cdfb091e1df046768
Rafael Fraga <tirafaelfraga@gmail.com>
2025-07-28 23:30:46 -0300

    feat: add reset password route

--- AuthService/AuthService.Api/Controllers/AuthController.cs
+++ AuthService/AuthService.Api/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
+using AuthService.Application.DTOs;
+using AuthService.Application.DTOs.Auth;
 using AuthService.Application.DTOs.Login;
-using AuthService.Application.DTOs.Request;
+using AuthService.Application.DTOs.User;
 using AuthService.Application.Services;
+using AuthService.Application.Services.Auth;
 using AuthService.Application.Services.Token;
 using DotnetBaseKit.Components.Api.Base;
 using DotnetBaseKit.Components.Api.Responses;
@@ -14,36 +17,50 @@ namespace AuthService.Api.Controllers;
 [Route("api/auth")]
 public class AuthController : ApiControllerBase
 {
-    private readonly IUserService _userService;
-    private readonly ITokenService  _tokenService;
+    private readonly IUserServiceApplication _userServiceApplication;
+    private readonly ITokenServiceApplication _tokenServiceApplication;
+    private readonly IAuthServiceApplication _authServiceApplication;
 
-    public AuthController(IResponseFactory responseFactory, IUserService userService, ITokenService tokenService)
+    public AuthController(IResponseFactory responseFactory, IUserServiceApplication userServiceApplication, ITokenServiceApplication tokenServiceApplication,
+        IAuthServiceApplication authServiceApplication)
         : base(responseFactory)
     {
-        _userService = userService;
-        _tokenService = tokenService;
+        _userServiceApplication = userServiceApplication;
+        _tokenServiceApplication = tokenServiceApplication;
+        _authServiceApplication = authServiceApplication;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] UserRequestDto request)
     {
-         await _userService.RegisterAsync(request);
-            
-         return ResponseCreated();
+        await _userServiceApplication.RegisterAsync(request);
+
+        return ResponseCreated();
     }
-    
+
+
+    [ProducesResponseType(typeof(TokenDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpPost("login")]
     public async Task<IActionResult> LoginAsync(LoginRequestDto dto)
     {
-        var authenticateUser = await _userService.AuthenticateAsync(dto);
+        var authenticateUser = await _userServiceApplication.AuthenticateAsync(dto);
         var userIsNull = authenticateUser == null;
         if (userIsNull)
         {
             return ResponseBadRequest(authenticateUser);
         }
 
-        var token = _tokenService.GenerateTokenAsync(authenticateUser!.Id, authenticateUser.Email);
+        var token = _tokenServiceApplication.GenerateTokenAsync(authenticateUser!.Id, authenticateUser.Email);
 
         return ResponseOk(token);
+    }
+
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPasswordAsync(ResetPasswordDto dto)
+    {
+        await _authServiceApplication.ResetPasswordAsync(dto.Email, dto.NewPassword);
+
+        return CreateResponse();
     }
 }
