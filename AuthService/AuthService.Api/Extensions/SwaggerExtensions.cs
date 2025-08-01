2d71c8b9c0a71952c63acd71745c5f5a704f02e3
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-01 17:53:24 -0300

    chore: remove authorization from Swagger

--- AuthService/AuthService.Api/Extensions/SwaggerExtensions.cs
+++ AuthService/AuthService.Api/Extensions/SwaggerExtensions.cs
@@ -11,35 +11,6 @@ public static class SwaggerExtensions
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mini Ecommerce", Version = "v1" });
 
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-            {
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                In = ParameterLocation.Header,
-                Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n" +
-                              "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-                              "Example: \"Bearer ey325sa3\""
-            });
-
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header
-                    },
-                    new List<string>()
-                }
-            });
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
             c.IncludeXmlComments(xmlPath);
