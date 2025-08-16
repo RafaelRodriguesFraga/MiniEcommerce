13f9ba6978df3e12ab109b9427773468ed1f11ec
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-15 22:14:51 -0300

    feat: add RepositoryExtensions

---
+++ UserService/UserService.Infra/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using UserService.Domain.Repositories;
+using UserService.Infra.Repositories;
+
+namespace UserService.Infra.Extensions;
+
+public static class RepositoryExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUserProfileReadRepository, UserProfileReadRepository>();
+        
+        return services;
+    }
+}
