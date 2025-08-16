cd26e81b52a73949a61b09cbd9aa46ac2b50a29e
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-15 22:16:42 -0300

    feat: add UserProfileResponseDto

---
+++ UserService/UserService.Application/DTOs/UserProfileResponseDto.cs
@@ -0,0 +1,27 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Application.DTOs;
+
+public class UserProfileResponseDto
+{
+    public UserProfileResponseDto(Guid id, string email, string name, string avatarUrl, DateTime createdAt)
+    {
+        Id = id;
+        Email = email;
+        Name = name;
+        AvatarUrl = avatarUrl;
+        CreatedAt = createdAt;
+    }
+
+    public Guid Id { get; }
+    public string Email { get; }
+    public string Name { get; }
+    public string AvatarUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+    public static implicit operator UserProfileResponseDto(UserProfile userProfile)
+    {
+        return new UserProfileResponseDto(userProfile.Id, userProfile.Name, userProfile.Email, userProfile.AvatarUrl,
+            userProfile.CreatedAt);
+    }
+}
