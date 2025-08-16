9fd635819b5a6277ebaa3373af7c529b98f1bf33
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-15 22:19:43 -0300

    feat: add configurations for bearer auth

--- UserService/UserService.Api/Program.cs
+++ UserService/UserService.Api/Program.cs
@@ -1,16 +1,42 @@
+using DotnetBaseKit.Components.Api;
+using DotnetBaseKit.Components.Application;
+using DotnetBaseKit.Components.Infra.Sql;
+using Microsoft.IdentityModel.Tokens;
+using UserService.Application.Extensions;
 using UserService.Infra.Context;
+using UserService.Infra.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddRepositories();
+builder.Services.AddApplicationServices();
 
-/* Put your database options here, e.g., using SQL Server
- builder.Services.AddDbContext<BoilerplateContext>(options =>
-     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); */
-builder.Services.AddDbContext<BoilerplateContext>();
+// BASEKIT DEPENDENCIES
+builder.Services.AddApi();
+builder.Services.AddApplication();
+
+builder.Services.AddDbContext<UserContext>(configuration);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Authority = "http://localhost:5194"; // URL do AuthService
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = true,
+            ValidIssuer = "MiniEcommerce.AuthService",
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -27,5 +53,7 @@ app.MapGet("/", context =>
 });
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
