6a07c20c0ce41df69590cd439f234d4a3326f13a
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-18 14:04:00 -0300

    feat: add CreateAsync

--- UserService/UserService.Api/Controllers/UserProfileController.cs
+++ UserService/UserService.Api/Controllers/UserProfileController.cs
@@ -4,16 +4,21 @@ using DotnetBaseKit.Components.Api.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UserService.Application;
+using UserService.Application.DTOs;
+using UserService.Shared.Filters;
 
 namespace UserService.Api.Controllers;
 
 [Route("api/user-profile")]
 [Authorize(AuthenticationSchemes = "Bearer")]
+[ApiController]
+[RequireUserId]
 public class UserProfileController : ApiControllerBase
 {
     private readonly IUserProfileServiceApplication _userProfileServiceApplication;
 
-    public UserProfileController(IResponseFactory responseFactory, IUserProfileServiceApplication userProfileServiceApplication) : base(
+    public UserProfileController(IResponseFactory responseFactory,
+        IUserProfileServiceApplication userProfileServiceApplication) : base(
         responseFactory)
     {
         _userProfileServiceApplication = userProfileServiceApplication;
@@ -22,14 +27,24 @@ public class UserProfileController : ApiControllerBase
     [HttpGet("me")]
     public async Task<IActionResult> GetMeAsync()
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (string.IsNullOrEmpty(userId))
-        {
-            return Unauthorized();
-        }
+        var userId = GetUserId();
+        var result = await _userProfileServiceApplication.GetByUserIdAsync(userId);
 
-        var result = await _userProfileServiceApplication.GetByUserIdAsync(Guid.Parse(userId));
-        
         return ResponseOk(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] UserProfileRequestDto dto)
+    {
+        var userId = GetUserId();
+        
+        var result = await _userProfileServiceApplication.CreateAsync(dto, userId);
+
+        return ResponseCreated(result);
+    }
+
+    private Guid GetUserId()
+    {
+        return (Guid) HttpContext.Items["UserId"]!;
     }
 }
