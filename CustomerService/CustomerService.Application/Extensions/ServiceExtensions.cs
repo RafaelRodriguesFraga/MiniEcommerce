7a7a2875bff7fd9148a27d620ede5a5307b3ad0d
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-27 11:32:06 -0300

    feat: add DI for address

--- CustomerService/CustomerService.Application/Extensions/ServiceExtensions.cs
+++ CustomerService/CustomerService.Application/Extensions/ServiceExtensions.cs
@@ -1,3 +1,5 @@
+using CustomerService.Application.Address;
+using CustomerService.Application.Services.Address;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CustomerService.Application.Extensions;
@@ -7,6 +9,8 @@ public static class ServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<ICustomerServiceApplication, CustomerServiceApplication>();
+        services.AddScoped<IAddressServiceApplication, AddressServiceApplication>();
+
         return services;
     }
 }
