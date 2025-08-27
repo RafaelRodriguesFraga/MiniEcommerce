62b005c6437b7a81f0f1a18df50216c75382efb6
Rafael Fraga <tirafaelfraga@gmail.com>
2025-08-27 11:31:49 -0300

    feat: add DI for address

--- CustomerService/CustomerService.Infra/Extensions/RepositoryExtensions.cs
+++ CustomerService/CustomerService.Infra/Extensions/RepositoryExtensions.cs
@@ -1,14 +1,18 @@
 using CustomerService.Domain.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using CustomerService.Infra.Repositories;
+using CustomerService.Infra.Repositories.Address;
 
 namespace CustomerService.Infra.Extensions;
+
 public static class RepositoryExtensions
 {
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
         services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+        services.AddScoped<IAddressWriteRepository, AddressWriteRepository>();
+        services.AddScoped<IAddressReadRepository, AddressReadRepository>();
 
         return services;
     }
