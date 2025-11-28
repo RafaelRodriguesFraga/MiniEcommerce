25b515664ccbd141e5e4e464132a6f28069f0af6
Rafael Fraga <tirafaelfraga@gmail.com>
2025-11-28 12:01:38 -0300

    refactor: restructure CustomerRequestDto constructor and properties

--- CustomerService/CustomerService.Application/DTOs/CustomerRequestDto.cs
+++ CustomerService/CustomerService.Application/DTOs/CustomerRequestDto.cs
@@ -5,7 +5,18 @@ namespace CustomerService.Application.DTOs;
 
 public class CustomerRequestDto : Notifiable<Notification>
 {
-    public string AvatarUrl { get; set; } = string.Empty;
+    public CustomerRequestDto(Guid authServiceId, string name, string email, string? avatarUrl = null)
+    {
+        AuthServiceId = authServiceId;
+        Name = name;
+        Email = email;
+        AvatarUrl = avatarUrl;
+    }
+
+    public Guid AuthServiceId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? AvatarUrl { get; set; }
 
 
     public void Validate()
