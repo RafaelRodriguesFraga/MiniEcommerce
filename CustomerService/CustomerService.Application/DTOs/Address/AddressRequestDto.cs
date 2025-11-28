7f49c9b1540552a989bbe93258035458d9f791fe
Rafael Fraga <tirafaelfraga@gmail.com>
2025-11-28 12:01:55 -0300

    refactor: remove CustomerId property from AddressRequestDto

--- CustomerService/CustomerService.Application/DTOs/Address/AddressRequestDto.cs
+++ CustomerService/CustomerService.Application/DTOs/Address/AddressRequestDto.cs
@@ -2,7 +2,6 @@ namespace CustomerService.Application.DTOs.Address
 {
     public class AddressRequestDto
     {
-        public Guid CustomerId { get; set; }
         public string Street { get; set; } = string.Empty;
         public string Number { get; set; } = string.Empty;
         public string Complement { get; set; } = string.Empty;
