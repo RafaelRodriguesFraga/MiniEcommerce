3e4f513e558e39781a3fc129ed95f745edf84b60
Rafael Fraga <tirafaelfraga@gmail.com>
2025-11-28 12:00:55 -0300

    feat: integrate IUserContext for user-specific address and customer operations

--- CustomerService/CustomerService.Api/Controllers/AddressController.cs
+++ CustomerService/CustomerService.Api/Controllers/AddressController.cs
@@ -1,8 +1,10 @@
+using System.Security.Claims;
 using CustomerService.Api.Documentation.Configuration;
 using CustomerService.Api.Documentation.Docs;
 using CustomerService.Api.Documentation.Docs.Keys;
 using CustomerService.Application.Address;
 using CustomerService.Application.DTOs.Address;
+using CustomerService.Application.Interfaces;
 using DotnetBaseKit.Components.Api.Base;
 using DotnetBaseKit.Components.Api.Responses;
 using Microsoft.AspNetCore.Authorization;
@@ -16,25 +18,32 @@ namespace CustomerService.Api.Controllers;
 public class AddressController : ApiControllerBase
 {
     private readonly IAddressServiceApplication _serviceApplication;
-    public AddressController(IResponseFactory responseFactory, IAddressServiceApplication serviceApplication) : base(responseFactory)
+    private readonly IUserContext _userContext;
+    public AddressController(IResponseFactory responseFactory, IAddressServiceApplication serviceApplication, IUserContext userContext) : base(responseFactory)
     {
         _serviceApplication = serviceApplication;
+        _userContext = userContext;
     }
 
     [HttpGet("{id:guid}")]
     [SwaggerDocumentation(typeof(AddressDocs), nameof(AddressDocKey.GetById))]
     public async Task<IActionResult> GetByIdAsync(Guid id)
     {
-        var address = await _serviceApplication.GetByIdAsync(id);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        var address = await _serviceApplication.GetByIdAsync(id, myUserId);
 
         return ResponseOk(address);
     }
 
-    [HttpGet("customer/{customerId:guid}")]
+    [HttpGet]
     [SwaggerDocumentation(typeof(AddressDocs), nameof(AddressDocKey.GetAllByCustomerId))]
-    public async Task<IActionResult> GetByCustomerIdAsync(Guid customerId)
+    public async Task<IActionResult> GetMyAddresses()
     {
-        var addresses = await _serviceApplication.GetByCustomerIdAsync(customerId);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        if (myUserId == Guid.Empty)
+            return Unauthorized();
+
+        var addresses = await _serviceApplication.GetByCustomerIdAsync(myUserId);
 
         return ResponseOk(addresses);
     }
@@ -43,8 +52,8 @@ public class AddressController : ApiControllerBase
     [SwaggerDocumentation(typeof(AddressDocs), nameof(AddressDocKey.Create))]
     public async Task<IActionResult> CreateAsync([FromBody] AddressRequestDto request)
     {
-
-        await _serviceApplication.CreateAsync(request);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        await _serviceApplication.CreateAsync(request, myUserId);
 
         return ResponseCreated();
     }
@@ -53,8 +62,8 @@ public class AddressController : ApiControllerBase
     [SwaggerDocumentation(typeof(AddressDocs), nameof(AddressDocKey.Update))]
     public async Task<IActionResult> Update(Guid id, [FromBody] AddressUpdateDto request)
     {
-
-        await _serviceApplication.UpdateAsync(id, request);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        await _serviceApplication.UpdateAsync(id, request, myUserId);
 
         return CreateResponse();
     }
@@ -63,7 +72,8 @@ public class AddressController : ApiControllerBase
     [SwaggerDocumentation(typeof(AddressDocs), nameof(AddressDocKey.Delete))]
     public async Task<IActionResult> Delete(Guid id)
     {
-        await _serviceApplication.DeleteAsync(id);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        await _serviceApplication.DeleteAsync(id, myUserId);
 
         return CreateResponse();
     }
--- CustomerService/CustomerService.Api/Controllers/CustomerController.cs
+++ CustomerService/CustomerService.Api/Controllers/CustomerController.cs
@@ -10,60 +10,57 @@ using CustomerService.Shared.Filters;
 using CustomerService.Api.Documentation.Configuration;
 using CustomerService.Api.Documentation.Docs;
 using CustomerService.Api.Documentation.Docs.Keys;
+using CustomerService.Application.Interfaces;
 
 namespace CustomerService.Api.Controllers;
 
 [Route("api/customers")]
 [Authorize(AuthenticationSchemes = "Bearer")]
 [ApiController]
-[RequireUserId]
 public class CustomerController : ApiControllerBase
 {
     private readonly ICustomerServiceApplication _customerServiceApplication;
+    private readonly IUserContext _userContext;
+
 
     public CustomerController(IResponseFactory responseFactory,
-        ICustomerServiceApplication customerServiceApplication) : base(
+        ICustomerServiceApplication customerServiceApplication,
+        IUserContext userContext) : base(
         responseFactory)
     {
         _customerServiceApplication = customerServiceApplication;
+        _userContext = userContext;
     }
 
-    [HttpGet("/me")]
+    [HttpPost("me")]
     [SwaggerDocumentation(typeof(CustomerDocs), nameof(CustomerDocKey.GetMe))]
-    public async Task<IActionResult> GetMeAsync()
-    {
-        var userId = GetAuthServiceId();
-        var result = await _customerServiceApplication.GetByUserIdAsync(userId);
 
-        return ResponseOk(result);
+    public async Task<IActionResult> GetMe()
+    {
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        if (myUserId == Guid.Empty)
+        {
+            return Unauthorized();
+        }
+
+        var result = await _customerServiceApplication.GetOrCreateAsync();
+
+        return Ok(result);
     }
 
-    [HttpPost]
-    [SwaggerDocumentation(typeof(CustomerDocs), nameof(CustomerDocKey.Create))]
-    public async Task<IActionResult> CreateAsync([FromBody] CustomerRequestDto dto)
-    {
-        var userId = GetAuthServiceId();
-        var userName = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Name)?.Value;
-        var userEmail = HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
-
-
-        var result = await _customerServiceApplication.CreateAsync(dto, userId, userName!, userEmail!);
-
-        return ResponseCreated(result);
-    }
-
-    [HttpPatch("{customerId:guid}")]
+    [HttpPatch("me")]
     [SwaggerDocumentation(typeof(CustomerDocs), nameof(CustomerDocKey.Update))]
-    public async Task<IActionResult> UpdateAsync([FromRoute] Guid customerId, [FromBody] CustomerUpdateDto dto)
+    public async Task<IActionResult> UpdateAsync([FromBody] CustomerUpdateDto dto)
     {
-        var authServiceId = GetAuthServiceId();
-        var result = await _customerServiceApplication.UpdateAsync(customerId, authServiceId, dto);
+        var myUserId = Guid.Parse(_userContext.UserId!);
+        if (myUserId == Guid.Empty)
+        {
+            return Unauthorized();
+        }
+
+        var result = await _customerServiceApplication.UpdateAsync(myUserId, dto);
 
         return ResponseOk(result);
     }
 
-    private Guid GetAuthServiceId()
-    {
-        return (Guid)HttpContext.Items["UserId"]!;
-    }
 }
