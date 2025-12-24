91c8fdc46f70b420816afc9f185d37b2e0beb210
Rafael Fraga <tirafaelfraga@gmail.com>
2025-12-24 10:56:33 -0300

    feat: add Sku to ProductRequestDto

--- ProductService/ProductService.Application/DTOs/ProductRequestDto.cs
+++ ProductService/ProductService.Application/DTOs/ProductRequestDto.cs
@@ -7,6 +7,7 @@ public class ProductRequestDto : Notifiable<Notification>
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
+    public string Sku { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
 }
