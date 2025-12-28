2c33dbd0bbf9f1e8c63d9fa8d860595daa7144cf
Rafael Fraga <tirafaelfraga@gmail.com>
2025-12-28 10:22:38 -0300

    fix: remove redundant Swagger configuration

--- CustomerService/CustomerService.Api/Program.cs
+++ CustomerService/CustomerService.Api/Program.cs
@@ -18,45 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniEcommerce - CustomerServiceApi", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 12345abcdef\"",
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-            },
-            new List<string>()
-        }
-    });
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-
-    c.OperationFilter<SwaggerDocumentationOperationFilter>();
-});
 
 builder.Services.AddControllers();
 builder.Services.AddRepositories();
@@ -66,6 +27,7 @@ builder.Services.AddHttpContextAccessor();
 // BASEKIT DEPENDENCIES
 builder.Services.AddApi();
 builder.Services.AddApplication();
+builder.Services.AddCustomSwagger();
 
 // PROJECT DEPENDENCIES
 builder.Services.AddDbContext<CustomerContext>(configuration);
