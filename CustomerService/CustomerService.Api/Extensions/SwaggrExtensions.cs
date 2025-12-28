321aee2ba4783b67e8ea59216d5647ee115e0d90
Rafael Fraga <tirafaelfraga@gmail.com>
2025-12-28 10:22:29 -0300

    feat: add custom Swagger extension

---
+++ CustomerService/CustomerService.Api/Extensions/SwaggrExtensions.cs
@@ -0,0 +1,49 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using Commons.Swagger.Configuration;
+
+namespace CustomerService.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniEcommerce - CustomerServiceApi", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description =
+                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 12345abcdef\"",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+
+            c.OperationFilter<SwaggerDocumentationOperationFilter>();
+        });
+
+        return services;
+    }
+}
