7a418b221994ae3e3d1c86056746687d25516ce5
Rafael Fraga <tirafaelfraga@gmail.com>
2026-01-26 17:18:54 -0300

    refactor:  remove swagger security definition

--- AuthService/AuthService.Api/Extensions/SwaggerExtensions.cs
+++ AuthService/AuthService.Api/Extensions/SwaggerExtensions.cs
@@ -12,86 +12,11 @@ public static class SwaggerExtensions
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniEcommerce - AuthServiceApi", Version = "v1" });
-
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-            {
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                In = ParameterLocation.Header,
-                Description =
-                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 12345abcdef\"",
-            });
-
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
-                    },
-                    new List<string>()
-                }
-            });
-
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            c.IncludeXmlComments(xmlPath);
-
+    
             c.OperationFilter<SwaggerDocumentationOperationFilter>();
 
         });
 
         return services;
-    }
-
-    private static void ConfigureSwaggerInfo(SwaggerGenOptions options)
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniEcommerce - AuthService.Api", Version = "v1" });
-    }
-
-    private static void ConfigureSwaggerSecurity(SwaggerGenOptions options)
-    {
-        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
-                          "Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\n" +
-                          "Example: \"Bearer 12345abcdef\"",
-        });
-
-        options.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new List<string>()
-            }
-        });
-    }
-
-    private static void ConfigureXmlComments(SwaggerGenOptions options)
-    {
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-        options.IncludeXmlComments(xmlPath);
     }
 }
