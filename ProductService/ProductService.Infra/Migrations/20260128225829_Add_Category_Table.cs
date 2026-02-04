df195982f1d3a1ef9a2179f1f9687e5edb6131d6
Rafael Fraga <tirafaelfraga@gmail.com>
2026-02-04 19:42:42 -0300

    feat: add category table

---
+++ ProductService/ProductService.Infra/Migrations/20260128225829_Add_Category_Table.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductService.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Category_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "products");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "category_id",
+                table: "products",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    slug = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    active = table.Column<bool>(type: "boolean", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_category_id",
+                table: "products",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_slug",
+                table: "categories",
+                column: "slug",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories",
+                table: "products",
+                column: "category_id",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories",
+                table: "products");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_category_id",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "category_id",
+                table: "products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "category",
+                table: "products",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
