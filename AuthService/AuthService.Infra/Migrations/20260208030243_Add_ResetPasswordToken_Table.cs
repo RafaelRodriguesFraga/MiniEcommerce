fa1b2fda49543d8792ec3b15de2fb3cb7b01c6f2
Rafael Fraga <tirafaelfraga@gmail.com>
2026-03-29 13:15:04 -0300

    feat: add reset password database migration

---
+++ AuthService/AuthService.Infra/Migrations/20260208030243_Add_ResetPasswordToken_Table.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ResetPasswordToken_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reset_password_tokens",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    token_hash = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    expiration_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    used = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reset_password_tokens", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_reset_password_tokens_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reset_password_tokens_token_hash",
+                table: "reset_password_tokens",
+                column: "token_hash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reset_password_tokens_user_id",
+                table: "reset_password_tokens",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reset_password_tokens");
+        }
+    }
+}
